da2f1fcd5bc5f6c97533b0038acc5ebf3f8665d8
shotaro tanaka <5511068t@gmail.com>
2021-07-16 01:42:45 +0900

    0716

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size: 16pt; color: #999;}
-h1 {font-size: 100pt; text-align: right; color: #eee;
-margin: -40px 0px -50px 0px;}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>This is sample page.</p>
-    <p>これは、サンプルで作ったページです。</p>
-</body>
-EOF;
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello/other', 'App\Http\Controllers\HelloController@other');
+Route::get('single', 'App\Http\Controllers\SingleController');
 
-Route::get('hello', function () use ($html){
-    return $html;
-});
