05deb46792a94d1ee36b8dc58042dd4a1e243989
jacoloves <5511068t@gmail.com>
2022-01-22 16:49:52 +0900

    Changes

---
+++ noDrunkCal/app/Http/Controllers/Chapter3HelloController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+
+class Chapter3HelloController extends Controller
+{
+    public function index()
+    {
+        //$data = [
+        //    ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
+        //    ['name'=>'Hanako Tanako', 'mail'=>'hanako@flower'],
+        //    ['name'=>'Sachiko Suzuki', 'mail'=>'sachivo@flower'],
+        //];
+        return view('hello.index', ['message'=>'Hello!']);
+    }
+
+//    public function post(Request $request)
+//    {
+//        return view('hello.index', ['msg'=>$request->msg]);
+//    }
+}
--- noDrunkCal/routes/web.php
+++ noDrunkCal/routes/web.php
@@ -12,7 +12,66 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; colot:#999; }
+h1 { font-size:100pt; text-align: right; color:#eee; margin: -40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello2', function () use ($html){
+    return $html;
+});
+
+Route::get('hello3/{msg?}', function ($msg='no message.'){
+    $html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; colot:#999; }
+h1 { font-size:100pt; text-align: right; color:#eee; margin: -40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+    return $html;
+});
+
+//Route::get('hello4', 'App\Http\Controllers\HelloController@index');
+//Route::get('hello4/other', 'App\Http\Controllers\HelloController@other');
+
+Route::get('hello6', 'App\Http\Controllers\HelloController@index');
+
+// chapter3
+
+Route::get('hello7', function() {
+    return view('hello.index');
+});
+
+Route::get('hello8', 'App\Http\Controllers\Chapter3HelloController@index');
+//Route::post('hello8', 'App\Http\Controllers\Chapter3HelloController@post');
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('hello', function(){
+    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
 });
