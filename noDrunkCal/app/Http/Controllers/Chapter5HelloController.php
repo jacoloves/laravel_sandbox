115ae1dbce3bc29ceaa0ad85eee10f2926df80e9
jacoloves <5511068t@gmail.com>
2022-01-28 09:40:41 +0900

    chapter5 page 183

---
+++ noDrunkCal/app/Http/Controllers/Chapter5HelloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Validator;
+use Illuminate\Support\Facades\DB;
+
+class Chapter5HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+    }
+}
