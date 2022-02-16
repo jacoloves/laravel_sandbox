1b72afe75d5ffe49a4d3bc67eda1db46a0ff0340
jacoloves <5511068t@gmail.com>
2022-02-16 23:01:11 +0900

    chapter6 page 255

--- noDrunkCal/app/Models/Person.php
+++ noDrunkCal/app/Models/Person.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use http\Env\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
     use HasFactory;
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
@@ -28,4 +38,11 @@ class Person extends Model
     {
         return $query->where('age', '<=', $n);
     }
+/*
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new ScopePerson);
+    }
+*/
 }
